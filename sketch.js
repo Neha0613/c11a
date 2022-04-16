9556d02dca8b2eafe3b9176eff8850d2f5cc65e1
Neha0613 <102873884+Neha0613@users.noreply.github.com>
2022-04-16 13:27:19 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+function weightAverage (){
+var sum=0
+for (var i =0; i<weight.length; i=i+1){
+  sum=sum+weight[i]
+}
+var average=sum/weight.length
+console.log(average)
+}
+
+
+function setup() {
+  createCanvas(400,400);
+weightAverage ()
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
